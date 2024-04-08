e5719623859250cf5dadb1b003fb05a002bdfbc6
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-04-08 22:34:13 +0500

    newupdate

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "inquirer";
+
+// printing a Wellcome Massege
+console.log("\n\twellcome to \ 'codeWithvandana\' - simple-calculator");
+
+// Asking Questions from users through inquirer
+
+
+const answer = await inquirer.prompt([
+{ message: "Enter first number", type: "number", name: "firstNumber" },
+{ message: "Enter second number", type: "number", name: "secondNumber" },
+{
+  message: "Select one of the operators to perform operator",
+  type: "list",
+  name: "operator",
+  choices: ["Addition", "Subtraction", "Multiplication", "Division",],
+},          
+]);
+
+// conditional statement
+  if (answer.operator === "Addition") {
+      console.log(answer.firstNumber + answer.secondNumber);  
+} else if (answer.operator === "Subtraction") {
+      console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+      console.log(answer.firstNumber * answer.secondNumber);     
+}
+ else if (answer.operator === "Division") {
+      console.log(answer.firstNumber / answer.secondNumber);
+ } else {
+      console.log("please select valid operator") 
+ }
